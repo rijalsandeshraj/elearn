a7a32b476b5ce5124784953a56dab0c28ad3cde2
Sandesh Rijal <sandesh.rijal@aayulogic.com>
2019-06-19 18:34:56 +0545

    Initial Commit

---
+++ courses/models.py
@@ -0,0 +1,115 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class CourseTest(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    members = models.ManyToManyField(User,
+                                     related_name='students_group')
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='courses_created',
+                              on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    group = models.ForeignKey(Group,
+                              related_name='group_study',
+                              on_delete=models.CASCADE)
+    duration = models.DurationField()
+    file = models.FileField(blank=True)
+    preview = models.ImageField(blank=True)
+    email = models.EmailField(max_length=200)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    test = models.OneToOneField(CourseTest,
+                                related_name='course_test',
+                                on_delete=models.CASCADE)
+    score = models.IntegerField(blank=True)
+    trending = models.BooleanField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
